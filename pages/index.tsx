1800b6419a765358ce826b65ebd9feb0ef86b186
WPaczula <wojciech.paczula@gmail.com>
2021-01-13 21:31:35 +0100

    feat: Add simple button common component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Head from 'next/head';
+import Button from 'components/button';
 
 const Home = () => (
   <div>
     <Head>
       <title>Home</title>
     </Head>
-    Welcome home
+    <Button onClick={() => console.log('Hello')}>Add</Button>
   </div>
 );
 
